18d444dabbad68e3958f9c0d4714210238310682
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-08 15:43:46 +0300

    test create user api

--- app/user/tests/test_users_api.py
+++ app/user/tests/test_users_api.py
@@ -14,3 +14,19 @@ def create_user(**params):
 
 class PublicUserApiTests(TestCase):
     """Test the users API (public)"""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'email': 'test@londonappdev.com',
+            'password': 'testpass',
+            'name': 'Test name'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status=status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
