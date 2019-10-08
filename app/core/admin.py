81ffc6d7a696acad55529c33325ea4aec57d0c4a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-08 10:58:15 +0300

    custom user model , add user page

--- app/core/admin.py
+++ app/core/admin.py
@@ -13,5 +13,11 @@ class UserAdmin(BaseUserAdmin):
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login',)})
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
 
 admin.site.register(models.User, UserAdmin)
