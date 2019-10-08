6a48835ec7be829bb9796c995d395ec959caa79d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-08 16:03:48 +0300

    building user serializer

---
+++ app/user/serializers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the users object"""
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'name')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        pass
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        pass
