f45dbc31882ed57de9896f9217acff50f6fe5682
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-09 09:23:03 +0300

    testing tags

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+class PublicTagsApiTests(TestCase):
+    """Test thje publicly available tags API"""
+    pass
